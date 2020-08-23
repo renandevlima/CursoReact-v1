b8f2e066f03701391a9638c9a525d84a15797c35
Renan Arantes Lima <renanlima@mac-de-renan.local>
2020-08-23 13:58:36 -0300

    Versão Final da Aula 1

--- src/components/basicos/CondicionalComIf.jsx
+++ src/components/basicos/CondicionalComIf.jsx
@@ -5,10 +5,10 @@ export default props => {
     return (
         <div>
             <h2>O número é: {props.numero}</h2>
-            <If teste={ props.numero % 2 == 0 }>
+            <If teste={ props.numero % 2 === 0 }>
                 <span>Par</span>
             </If>
-            <If teste={ props.numero % 2 == 1 }>
+            <If teste={ props.numero % 2 === 1 }>
                 <span>Ímpar</span>
             </If>
         </div>
