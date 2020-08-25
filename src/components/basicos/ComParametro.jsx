1a85956c48b4350ba8eb764c3680cf438765c193
Renan Arantes Lima <renanlima@mac-de-renan.local>
2020-08-25 16:48:25 -0300

    finalizada segunda aula

--- src/components/basicos/ComParametro.jsx
+++ src/components/basicos/ComParametro.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
-export default props =>
-    <>
-        <h3>{props.titulo}</h3>
-        <p>{props.subtitulo}</p>
-    </>
+export default props => {
+    // props é somente leitura!!
+    // props.titulo = 'Outro Titulo';
+    return (
+        <>
+            <h3>{props.titulo}</h3>
+            <p>{props.subtitulo}</p>
+        </>
+    )
+}
